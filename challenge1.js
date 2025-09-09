4e33738096193ec4c9b9fff5435fc2edf767d91e
habiba hussein <habiba.hussein@moringaschool.comclear>
2025-09-09 23:56:00 +0300

    weakly code challenge

---
+++ challenge1.js
@@ -0,0 +1,23 @@
+const marks = 90;  
+
+if (marks > 79 && marks <= 100) {
+  console.log("Grade: A");
+} else if (marks >= 60 && marks <= 79) {
+  console.log("Grade: B");
+} else if (marks >= 50 && marks <= 59) {
+  console.log("Grade: C");
+} else if (marks >= 40 && marks <= 49) {
+  console.log("Grade: D");
+} else if (marks >= 0 && marks < 40) {
+  console.log("Grade: E");
+} else {
+  console.log("Grade.");
+}
+
+    
+  
+
+
+
+
+
